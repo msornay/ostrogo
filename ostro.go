340f5c3322c42b45afe22907f9a1ca1f64c5282d
Mathieu Sornay <mathieu.sornay@ercom.fr>
2016-02-04 15:59:57 +0100

    [proxy] "working" http proxy

--- ostro.go
+++ ostro.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -16,12 +17,12 @@ func printRaw(req *http.Request) {
 	fmt.Printf("%s\n", raw)
 }
 
-type OstrogoProxy struct {
+type Proxy struct {
 	Client http.Client
 }
 
 // See https://golang.org/src/net/http/httputil/reverseproxy.go
-func (p *OstrogoProxy) Serve(rw http.ResponseWriter, in *http.Request) {
+func (p *Proxy) Serve(rw http.ResponseWriter, in *http.Request) {
 	out := new(http.Request)
 	*out = *in // Shallow copy apparently
 
@@ -29,6 +30,9 @@ func (p *OstrogoProxy) Serve(rw http.ResponseWriter, in *http.Request) {
 	out.RequestURI = ""
 	out.URL.Scheme = "http"
 	out.URL.Host = in.Host // From the Host header
+
+	// XXX Refuse localhost & co.
+
 	out.URL.Path = in.URL.Path
 
 	log.Println("Forwarding: ", out.URL)
@@ -40,20 +44,34 @@ func (p *OstrogoProxy) Serve(rw http.ResponseWriter, in *http.Request) {
 		log.Println("Do: ", err)
 		return
 	}
-	fmt.Println(resp)
-	// rw.WriteHeader(resp.Status)
-	// b, err := resp.Body.Read()
-	// if err != nil {
-	//	log.Println("Do: ", err)
-	//	return
-	// }
-	resp.Write(rw)
+
+	var rh http.Header = rw.Header()
+	for key, values := range resp.Header {
+		for _, v := range values {
+			rh.Add(key, v)
+		}
+	}
+
+	rw.WriteHeader(resp.StatusCode)
+
+	b := make([]byte, 16)
+	defer resp.Body.Close()
+	for {
+		n, err := resp.Body.Read(b)
+		rw.Write(b[:n])
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println("Body.Read: ", err)
+			return
+		}
+	}
 }
 
 func main() {
-	p := new(OstrogoProxy)
+	p := new(Proxy)
 	http.HandleFunc("/", p.Serve)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
